9e126f6a6c65bd26ae52e3943cc3f2048de900dd
Cristian Ramirez <ramirezcristian81@live.com>
2020-09-10 15:28:37 -0700

    signIn page added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+
 function App() {
   return (
     <div>
+      <Header></Header>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signIn" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./sign-in-sign-up.style.scss";
+import SignIn from "../../components/sign-in/sign-in.componet";
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div
+      className="sign-in-and-sign-up
+  "
+    >
+      <SignIn></SignIn>
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
