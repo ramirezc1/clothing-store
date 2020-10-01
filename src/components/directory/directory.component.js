bc96b37b9d034d54962ceef8d98a71691f214dd9
Cristian Ramirez <ramirezcristian81@live.com>
2020-10-01 13:45:45 -0700

    moved directory and shop data into its own reducer

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,24 +1,23 @@
-import React, { Component } from "react";
-import "./directory.styles.scss";
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+
 import MenuItem from "../menu-item/menu-item.component";
-import SECTIONS_DATA from "./sections.data.js";
 
-class Directory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sections: SECTIONS_DATA,
-    };
-  }
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps}></MenuItem>
-        ))}
-      </div>
-    );
-  }
-}
+import "./directory.styles.scss";
 
-export default Directory;
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
+
+export default connect(mapStateToProps)(Directory);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
 
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App></App>
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
