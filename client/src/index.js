eaa7c4c67e32ea4ad396d4d6b920a183ce915c42
Cristian Ramirez <ramirezcristian81@live.com>
2020-11-12 15:34:49 -0800

    updating app to be pwa compliant

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/lib/integration/react";
-import * as serviceReducer from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
